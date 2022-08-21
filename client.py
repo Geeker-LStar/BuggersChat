33b9771be90ec7ca549c45abf69f8638e01cf3d6
李天星 Ella <103357243+Geeker-LStar@users.noreply.github.com>
2022-08-21 20:37:27 +0800

    Add files via upload
    Version 1.0

---
+++ client.py
@@ -0,0 +1,52 @@
+import tkinter as tk
+import time
+import socket
+import threading
+
+# 服务端发送的消息
+def Send_Message():
+    result = GulSlk.get("1.0", "end")
+    s.send(result.encode())
+    theTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    GulListBox.insert("end", "客户端" + "     " + theTime + ":")
+    GulListBox.insert("end", "             " + result)
+    GulSlk.delete("1.0", "end")
+
+# 接收消息
+def Receive_Sever(s):
+    while True:
+        info=s.recv(1024).decode()
+        theTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        GulListBox.insert("end", "服务端" + "     " + theTime + ":")
+        GulListBox.insert("end", "             " + info)
+
+def Client(s):
+ 
+    s.connect((host, post))
+    print('连接成功')
+    Threading_one = threading.Thread(target=Receive_Sever, args=(s,))
+    Threading_one.daemon = True
+    Threading_one.start()
+ 
+ 
+if __name__ == '__main__':
+    window = tk.Tk()
+ 
+    window.geometry('600x600')
+    window.title('客户端')
+ 
+    GulListBox = tk.Listbox(window, width=120, height=20)
+    GulListBox.pack()
+ 
+    GulButton = tk.Button(window, width=10, text="发送", command=Send_Message)
+    GulButton.place(x=500, y=550)
+ 
+    GulSlk = tk.Text(window, width=120, height=10)
+    GulSlk.place(x=10, y=400)
+    host = socket.gethostname()
+    post = 1320
+    s = socket.socket()
+    threding1=threading.Thread(target=Client,args=(s,))
+    threding1.start()
+ 
+    window.mainloop()
